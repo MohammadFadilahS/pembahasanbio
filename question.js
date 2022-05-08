a8ab670438a2163fa145db6726625bed6c231db7
MohammadFadilahS <masfadil001@gmail.com>
2022-05-08 09:11:03 +0700

    berhasil kotak quiz

---
+++ question.js
@@ -0,0 +1,15 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        question: "Berikut ini yang merupakan komponen biotik penyusun ekosistem ?",
+        answer: "b. dekomposer",
+        options: [
+            "a. pH",
+            "b. dekomposer",
+            "c. Oksigen",
+            "d. Iklim"
+
+        ]
+
+    },
+];
